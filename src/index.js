4ef74dec92958aaf33d84c45d3ebbfe0d66ce539
yannnaaa <yannnaaa@mail.ru>
2019-10-14 19:59:30 +0300

    add function zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,68 @@
 module.exports = function zeros(expression) {
-  // your solution
+        
+  let count2 = 0;
+  let count5 = 0;
+  let c2 = 0;
+  let c5 = 0;
+  
+  let arr = expression.split('*');
+      
+  for (let i = 0; i < arr.length; i++) {
+    
+    let b = +parseInt(arr[i]);
+    
+    if (arr[i].includes('!!')) {      
+      
+      if ( b % 2 === 0 ) { // chetn
+        let c = b / 2;       
+        
+        count2 += c;
+        
+        factorial(c); 
+       
+        count2 += c2;
+        count5 += c5;          
+        
+      } else {		// nechetn      
+        let d = ( b - 1 ) / 2;
+        
+        factorial(b);
+        
+        count5 += c5;
+        
+        factorial(d);
+        
+        count5 -= c5;            
+      }
+           
+    } else {     
+      factorial(b); 
+      
+      count2 += c2;
+      count5 += c5; 
+    }
+  }
+     
+  function factorial(expr) {
+    
+    let a = +expr;
+    c2 = 0;
+    c5 = 0;
+    
+    while ( Math.floor(a) > 0) {                
+      a = Math.floor( a / 5 ); 
+      c5 += a; 
+    } 
+    
+    a = +expr;
+    
+    while ( Math.floor(a) > 0) {               
+      a = Math.floor( a / 2 );
+      c2 += a; 
+    }
+    
+    return c2, c5;  
+  }
+  
+  return count2 > count5 ? count5 : count2; 
 }
